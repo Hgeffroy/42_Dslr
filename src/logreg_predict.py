ef37883179fd324d94aa46e9856ef7db1d86fcf7
hgeffroy <henri.geffroy@gmail.com>
2025-08-08 13:28:18 +0200

    feat: added parser to training and predicting scripts

--- src/logreg_predict.py
+++ src/logreg_predict.py
@@ -1,12 +1,34 @@
 from classes.logistic_model import LogisticModel
 from classes.dataset import Dataset
 from utils import get_path
+import argparse
 
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d", "--data",
+        type=str,
+        default=get_path("datasets/dataset_test.csv"),
+        help="The path to the data to predict CSV."
+    )
+
+    parser.add_argument(
+        "-m", "--model",
+        type=str,
+        default=get_path("models/models.csv"),
+        help="The path to the model CSV."
+    )
+
+    return parser
 
 def main():
-    dataset = Dataset(get_path("datasets/dataset_train.csv"))
+    parser = build_parser()
+    args = parser.parse_args()
+    dataset = Dataset(args.data)
     model = LogisticModel()
-    model.predict(get_path("models/models.csv"), dataset)
+    model.predict(args.model, dataset)
 
 
 if __name__ == "__main__":
--- src/logreg_train.py
+++ src/logreg_train.py
@@ -1,12 +1,35 @@
 from classes.logistic_model import LogisticModel
 from classes.dataset import Dataset
 from utils import get_path
+import argparse
+
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d", "--data",
+        type=str,
+        default=get_path("datasets/dataset_train.csv"),
+        help="The path to the data training CSV."
+    )
+
+    parser.add_argument(
+        "-f", "--features",
+        type=str,
+        default="Herbology,Defense Against the Dark Arts",
+        help="Features to use for training separated by a comma.",
+    )
+
+    return parser
 
 
 def main():
-    dataset = Dataset(get_path("datasets/dataset_train.csv"))
+    parser = build_parser()
+    args = parser.parse_args()
+    dataset = Dataset(args.data)
     model = LogisticModel()
-    model.train(dataset, ['Herbology', 'Defense Against the Dark Arts'])
+    model.train(dataset, args.features.split(","))
 
 
 if __name__ == "__main__":
